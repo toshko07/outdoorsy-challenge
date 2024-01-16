840aead7e41074b5836eea9a32ea049d256f87c1
Todor Tabakov <t.tabakov94@gmail.com>
2024-01-16 10:24:18 +0200

    Initialize echo server

---
+++ cmd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"github.com/toshko07/outdoorsy-challenge/internal/configs"
+	"github.com/toshko07/outdoorsy-challenge/internal/controllers"
+	"github.com/toshko07/outdoorsy-challenge/internal/db"
+	"github.com/toshko07/outdoorsy-challenge/internal/repositories"
+	"github.com/toshko07/outdoorsy-challenge/internal/services"
+)
+
+func main() {
+	// Setup
+	cfg := configs.LoadConfig()
+	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
+
+	// Database
+	database := db.Connect(cfg.DB)
+
+	// Repositories
+	rentalsRepo := repositories.NewRentalsImpl(database)
+
+	// Services
+	rentalsService := services.NewRentalsImpl(rentalsRepo)
+
+	// Controllers
+	rentalsController := controllers.NewRentalsController(rentalsService)
+
+	v1 := e.Group("/v1")
+	v1.GET("/rentals/:rental_id", rentalsController.GetRental)
+
+	// Start server
+	go func() {
+		if err := e.Start(fmt.Sprintf(":%d", cfg.ServerPort)); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatalf("failed to start server: %v", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
